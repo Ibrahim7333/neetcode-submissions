2897a170000dad85a760c30db98c4a4a20b225dd
Ibrahim Abdul Rehman <86287483+Ibrahim7333@users.noreply.github.com>
2026-07-12 15:16:31 +0500

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.js
@@ -0,0 +1,45 @@
+class Solution {
+  
+    // isAnagram(s, t) 
+    // {
+    //     const map = new Map();
+
+    //     if (s.length !== t.length) return false;
+
+    //     for (let i=0; i<s.length; i++)
+    //     {
+    //         const oldSMapValue = map.get(s[i]) ?? 0;
+    //         map.set(s[i],oldSMapValue+1);
+    //         const oldTMapValue = map.get(t[i]) ?? 0;
+    //         map.set(t[i],oldTMapValue-1);
+    //     }
+
+    //     for (let j=0; j<map.size; j++)
+    //     {
+    //         const getMapValue = map.get(s[j]) ?? 0;
+    //         if (getMapValue !== 0) return false;
+    //     }
+
+    //     return true;
+    // }
+
+    isAnagram(str1, str2)
+    {
+        let map = new Map();
+
+        if (str1.length !== str2.length) return false;
+
+        for (let i=0; i<str1.length; i++)
+        {
+            map.set(str1[i], (map.get(str1[i]) ?? 0) + 1 );
+            map.set(str2[i], (map.get(str2[i]) ?? 0) - 1 );
+        }
+
+        for (const [key, value] of map)
+        {
+            if (value !== 0) return false;
+        }
+
+        return true;
+    }
+}
