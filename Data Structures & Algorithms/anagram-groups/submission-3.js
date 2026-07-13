6fcfe7f5d76bdd05bf4fbbc13eee749fa04fcf87
Ibrahim Abdul Rehman <86287483+Ibrahim7333@users.noreply.github.com>
2026-07-13 16:48:44 +0500

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,42 @@
+class Solution {
+
+    // groupAnagrams(strs) 
+    // {
+    //     const res = {};
+    //     for (let s of strs) 
+    //     {
+    //         const count = new Array(26).fill(0);
+    //         for (let c of s) 
+    //         {
+    //             count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+    //         }
+    //         //console.log("count array: ",count)
+    //         const key = count.join(',');
+    //         //console.log("key", key)
+    //         if (!res[key]) 
+    //         {
+    //             res[key] = [];
+    //         }
+    //         res[key].push(s);
+    //         //console.log("map: ",res)
+    //     }
+    //     return Object.values(res);
+    // }
+
+
+    groupAnagrams(strs) 
+    {
+        const res = {};
+        for (let s of strs) 
+        {
+            const sortedS = s.split('').sort().join('');
+            if (!res[sortedS]) 
+            {
+                res[sortedS] = [];
+            }
+            res[sortedS].push(s);
+        }
+        //console.log("map: ",res)
+        return Object.values(res);
+    }
+}
