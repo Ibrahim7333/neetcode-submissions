dfe36fa22d9d8cb83749554e85d4a30ca9f16a50
Ibrahim Abdul Rehman <86287483+Ibrahim7333@users.noreply.github.com>
2026-07-14 14:46:14 +0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,54 @@
+class Solution {
+   
+    // topKFrequent(nums, k) 
+    // {
+    //     const freqMap = {};
+    //     for (let element of nums) 
+    //     {
+    //         freqMap[element] = (freqMap[element] || 0) + 1;
+    //     }
+    //     const uniqueElements = Object.keys(freqMap);
+        
+    //     uniqueElements.sort((a, b) => freqMap[b] - freqMap[a]);
+        
+    //     return uniqueElements.slice(0, k).map(Number);
+    // }
+
+    topKFrequent(nums, k) 
+    {
+        const freqMap = {};
+        const freqArr = new Array(nums.length + 1);
+        
+        for (let element of nums) 
+        {
+            freqMap[element] = (freqMap[element] || 0) + 1;
+        }
+        
+        for (const keys in freqMap)
+        {
+            if (!freqArr[freqMap[keys]])
+            {
+                freqArr[freqMap[keys]] = [];
+            }
+            freqArr[freqMap[keys]].push(keys);
+        }
+
+        
+        const outputArr = [];
+        
+        for (let i=freqArr.length-1; i >= 0; i--)
+        {
+            if (!freqArr[i]) { continue; }
+            
+            for (let j=freqArr[i].length-1; j >= 0; j--)
+            {
+                if (outputArr.length < k)
+                {
+                    outputArr.push(freqArr[i][j]);
+                }
+            }
+        }
+        
+        return outputArr;
+    }
+}
